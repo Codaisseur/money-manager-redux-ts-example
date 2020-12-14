28534f1205efba42334f46021c517449f63bc123
Domenico Gemoli <domenicogemoli@gmail.com>
2020-12-14 16:51:20 +0100

    Add reducer case

--- src/store/balance/reducer.ts
+++ src/store/balance/reducer.ts
@@ -7,6 +7,12 @@ const initialState: BalanceState = {
 
 export default function reducer(state = initialState, action: BalanceAction) {
   switch (action.type) {
+    case "balance/deposit": {
+      return {
+        ...state,
+        amount: state.amount + action.payload,
+      };
+    }
     default: {
       return state;
     }
