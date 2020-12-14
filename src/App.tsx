bb30914750b363596b003e8c6f0c1612d35c635c
Domenico Gemoli <domenicogemoli@gmail.com>
2020-12-14 17:00:13 +0100

    Add "Deposit custom amount" button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectBalance } from "./store/balance/selectors";
 import { deposit, withdraw, reset } from "./store/balance/actions";
@@ -5,6 +6,8 @@ import { deposit, withdraw, reset } from "./store/balance/actions";
 function App() {
   const dispatch = useDispatch();
   const balance = useSelector(selectBalance);
+
+  const [customAmount, setCustomAmount] = useState(0);
 
   return (
     <div className="App">
@@ -29,6 +32,24 @@ function App() {
         }}
       >
         Reset
+      </button>
+      <br />
+      <br />
+      <input
+        type="number"
+        value={customAmount}
+        onChange={e => {
+          setCustomAmount(parseInt(e.target.value));
+        }}
+      />
+      <br />
+      <button
+        onClick={() => {
+          dispatch(deposit(customAmount));
+          setCustomAmount(0);
+        }}
+      >
+        Deposit custom amount
       </button>
     </div>
   );
